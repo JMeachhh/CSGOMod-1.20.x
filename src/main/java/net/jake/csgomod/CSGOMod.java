87a804140415c7f8e8c2e8b2fb647381ed3d1e2a
Jake <jrmoo7@sky.com>
2024-08-19 17:12:04 +0100

    Added Butterfly Knife

--- src/main/java/net/jake/csgomod/CSGOMod.java
+++ src/main/java/net/jake/csgomod/CSGOMod.java
@@ -1,6 +1,9 @@
 package net.jake.csgomod;
 
 import com.mojang.logging.LogUtils;
+import net.jake.csgomod.item.ModCreativeModTabs;
+import net.jake.csgomod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -25,6 +28,9 @@ public class CSGOMod
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -41,10 +47,12 @@ public class CSGOMod
 
     }
 
-    // Add the example block item to the building blocks tab
+    // Add the custom block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.COMBAT){
+            event.accept(ModItems.BUTTERFLY_KNIFE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/jake/csgomod/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.jake.csgomod.item;
+
+import net.jake.csgomod.CSGOMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, CSGOMod.MOD_ID);
+
+    public static final RegistryObject<Item> BUTTERFLY_KNIFE = ITEMS.register("butterfly_knife",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
